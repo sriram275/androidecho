4a240fe197e4e56cfae208db244ae813337a84c3
sriram275 <sriram1275@gmail.com>
2016-07-23 11:55:20 +0530

    adding testing text

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -12,7 +12,7 @@ public class Echo extends CordovaPlugin {
         if (action.equals("show")) {
 
             String name = data.getString(0);
-            String message = "Hello, " + name;
+            String message = "Testing..Hello, " + name;
             callbackContext.success(message);
 
             return true;
