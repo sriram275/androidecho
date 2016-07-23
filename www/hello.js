a49763fe5a58fc2934bf0a2f1f0aa06a6c516546
sriram275 <sriram1275@gmail.com>
2016-07-23 16:09:35 +0530

    simple

--- www/hello.js
+++ www/hello.js
@@ -1,18 +1,5 @@
-cordova.define("com.example.hello.hello", function(require, exports, module) {
-
-//var argscheck = require('cordova/argscheck'),
-//    utils = require('cordova/utils'),
-//    exec = require('cordova/exec'),
-//    channel = require('cordova/channel');
-
-
-var Hello = function() {
+module.exports = {
+    show: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "show", [name]);
+    }
 };
-
-Hello.show = function(name,successCallback,errorCallback) {
-    exec(successCallback, errorCallback, "Hello", "show", [name]);
-};
-
-module.exports = Hello;
-
-});
