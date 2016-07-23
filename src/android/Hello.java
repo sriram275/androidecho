68d476290b855070d5fcb031ae8dc02959edc045
sriram275 <sriram1275@gmail.com>
2016-07-23 17:16:13 +0530

    list apps #1

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -13,7 +13,8 @@ public class Hello extends CordovaPlugin {
 
             String name = data.getString(0);
             String message = "Testing..Hello, " + name;
-            callbackContext.success(message);
+            ArrayList<PInfo> apps = getInstalledApps(false); /* false = no system packages */
+            callbackContext.success(apps);
 
             return true;
 
@@ -23,4 +24,53 @@ public class Hello extends CordovaPlugin {
 
         }
     }
+    
+//    public static void trimCache(Context context) {
+//        File dir = context.getCacheDir();
+//        if(dir!= null && dir.isDirectory()){
+//            File[] children = dir.listFiles();
+//            if (children == null) {
+//                // Either dir does not exist or is not a directory
+//            } else {
+//                File temp;
+//                for (int i = 0; i < children.length; i++) {
+//                    temp = children[i];
+//                    temp.delete();
+//                }
+//            }
+//        }
+//    } 
+    
+ class PInfo {
+    private String appname = "";
+    private String pname = "";
+    private String versionName = "";
+    private int versionCode = 0;
+    private Drawable icon;
+}
+
+//private ArrayList<PInfo> getPackages() {
+//    ArrayList<PInfo> apps = getInstalledApps(false); /* false = no system packages */
+//    return apps;
+//}
+
+private ArrayList<PInfo> getInstalledApps(boolean getSysPackages) {
+    ArrayList<PInfo> res = new ArrayList<PInfo>();        
+    List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
+    for(int i=0;i<packs.size();i++) {
+        PackageInfo p = packs.get(i);
+        if ((!getSysPackages) && (p.versionName == null)) {
+            continue ;
+        }
+        PInfo newInfo = new PInfo();
+        newInfo.appname = p.applicationInfo.loadLabel(getPackageManager()).toString();
+        newInfo.pname = p.packageName;
+        newInfo.versionName = p.versionName;
+        newInfo.versionCode = p.versionCode;
+        newInfo.icon = p.applicationInfo.loadIcon(getPackageManager());
+        res.add(newInfo);
+    }
+    return res; 
+}
+    
 }
